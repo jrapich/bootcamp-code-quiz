15df2671de9cc44cb17436ce7045cb54fbbff3bb
jeremysr <jeremysr@protonmail.com>
2023-07-08 11:39:02 -0600

    added eventlistener to initial button and laid out some functions in that event of what I want to happen

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+//lets declare all the things
+var startButton = document.querySelector(".confirm");
+var displayedQuestion = document.querySelector(".questions");
+var displayedAnswers = document.querySelector(".answers");
+
+
+//event listener for when user clicks the begin quiz button
+//will display a question and a list of answers
+startButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    hideDefaultText();
+    startTimer();
+    displayedQuestion.textContent = randomQuestion();
+    displayedAnswers.textContent = listAnswers();
+})
