fd76f86abb5dce6b790ad4940bfd212c81065e48
jeremysr <jeremysr@protonmail.com>
2023-07-08 13:24:44 -0600

    figuring out the proper way to show and hide stuff with CSS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,23 @@
 //lets declare all the things
 var startButton = document.querySelector(".confirm");
-var displayedQuestion = document.querySelector("#questionsDefault");
-var displayedAnswers = document.querySelector("#answersDefault");
+var defaultQuestions = document.querySelector("#questionsDefault");
+var defaultAnswers = document.querySelector("#answersDefault");
+var displayedQuestion = document.querySelector("#questionsDisplay");
+var displayedAnswers = document.querySelector("#answersDisplay");
 
+//function to hide the default text on starting page, an replace it with questions/answers
+function hideDefaultText() {
+    if (defaultQuestions.style) {}
+}
 
 //event listener for when user clicks the begin quiz button
 //will display a question and a list of answers
 startButton.addEventListener("click", function(event) {
     event.preventDefault();
-    hideDefaultText();
-    startTimer();
     displayedQuestion.textContent = randomQuestion();
     displayedAnswers.textContent = listAnswers();
+    hideDefaultText();
+    startTimer();
 })
 
-console.log(displayedQuestion);
+
