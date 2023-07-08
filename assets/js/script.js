6ec9b9a01b2116c947df8f9233474463c3acfea3
jeremysr <jeremysr@protonmail.com>
2023-07-08 13:37:55 -0600

    addeed logic to display/hide the default elements; added basic timer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,17 +7,31 @@ var displayedAnswers = document.querySelector("#answersDisplay");
 
 //function to hide the default text on starting page, an replace it with questions/answers
 function hideDefaultText() {
-    if (defaultQuestions.style) {}
+    defaultQuestions.setAttribute("style", "display:none");
+    defaultAnswers.setAttribute("style", "display:none");
+    displayedQuestion.setAttribute("style", "display:block");
+    displayedAnswers.setAttribute("style", "display:block");
+}
+
+//start the quiz timer
+function startTimer() {
+    var timer = 120
+    var timerInterval = setInterval(function() {
+        timer--;
+        if(timer === 0) {
+          clearInterval(timerInterval);
+        }
+    }, 1000);
 }
 
 //event listener for when user clicks the begin quiz button
 //will display a question and a list of answers
 startButton.addEventListener("click", function(event) {
     event.preventDefault();
-    displayedQuestion.textContent = randomQuestion();
-    displayedAnswers.textContent = listAnswers();
     hideDefaultText();
     startTimer();
+    displayedQuestion.textContent = randomQuestion();
+    displayedAnswers.textContent = listAnswers();
 })
 
 
